8b1875b7fb90fe13a17e1508f2061fe8c1888347
lukarion12 <lucass_1202@hotmail.com.br>
2018-11-21 01:52:27 -0200

    ////

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::post('/categorias/editar/{id}', 'CategoriaController@update');
 Route::post('/categorias', 'CategoriaController@store');
 Route::get('/categorias/restaurar', 'CategoriaController@indexWithTrashed');
 Route::get('/categorias/restaurar/{id}', 'CategoriaController@restore');
+///
 
 Route::get('/produtos', 'ProdutoController@index');
 Route::get('/produtos/novo', 'ProdutoController@create');
@@ -35,6 +36,7 @@ Route::get('/produtos/editar/{id}', 'ProdutoController@edit');
 Route::get('/produtos/apagar/{id}', 'ProdutoController@destroy');
 Route::post('/produtos', 'ProdutoController@store');
 Route::post('/produtos/editar/{id}','ProdutoController@update');
+///
 
 Route::get('/clientes', 'ClienteController@index');
 Route::get('/clientes/novo', 'ClienteController@create');
@@ -42,6 +44,7 @@ Route::get('/clientes/editar/{id}', 'ClienteController@edit');
 Route::get('/clientes/apagar/{id}', 'ClienteController@destroy');
 Route::post('/clientes','ClienteController@store');
 Route::post('/clientes/editar/{id}', 'ClienteController@update');
+///
 
 Route::get('/pedido-produto', function(){
     $pedidos = Pedido::with('produtos')->get();
